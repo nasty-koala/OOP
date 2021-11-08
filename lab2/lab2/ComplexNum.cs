6620c632fa4bf620d8cd3e05706a525cfca90bbc
nasty-koala <nasty16040224@gmail.com>
2021-11-08 11:51:23 +0500

    lab2

---
+++ lab2/lab2/ComplexNum.cs
@@ -0,0 +1,30 @@
+﻿namespace lab2
+{
+    public class Complex
+    {
+        private readonly double real;
+        private readonly double imag;
+
+        public Complex(double real_num, double imag_num)
+        {
+            real = real_num;
+            imag = imag_num;
+        }
+
+        public static Complex operator +(Complex c1, double c2) => new Complex(c1.real + c2, c1.imag);
+        public static Complex operator +(double c1, Complex c2) => c1 + c2;
+        
+        public static Complex operator -(Complex c1, double c2) => new Complex(c1.real - c2, c1.imag);
+        public static Complex operator -(double c1, Complex c2) => c1 - c2;
+        
+        public static Complex operator *(Complex c1, double c2) => new Complex(c1.real * c2, c1.imag * c2);
+        public static Complex operator *(double c1, Complex c2) => c1 * c2;
+        
+        public override string ToString()
+        {
+            if (imag<0) return $"{real} {imag}i";
+            return $"{real} + {imag}i";
+        }
+
+    }
+}
---
+++ lab2/lab2/Menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace lab2
+{
+    public class Menu
+    {
+        public static string NewMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\n\tМеню действий\n" + 
+                              "Для выбора введите кодовое слово, соответствующее команде (указано в скобках) и следуйте инструкциям\n\n" +
+                              "  -  Сложение(plus)\n" +
+                              "  -  Вычитание(minus)\n" +
+                              "  -  Умножение(mult)\n" +
+                              "  -  Очистить консоль (clean)\n" +
+                              "  -  Выход(exit)\n");
+            return Console.ReadLine();
+        }
+
+        public static Complex NewComplex()
+        {
+            Console.WriteLine("");
+        }
+
+        public static void MainMenu()
+        {
+            switch (NewMenu())
+            {
+                case "plus":
+
+                case "minus":
+                    
+                case "mult":
+
+                case "clean":
+                    
+                case "exit":
+                    Console.WriteLine("До свидвния! ((((((");
+                    Thread.Sleep(100);
+                    break;
+                default:
+                    Console.WriteLine("Неопознанная команда!");
+                    Console.ReadKey();
+                    MainMenu();
+                    break;
+            }
+        }
+    }
+}
